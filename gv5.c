36732c8c4dbfabaa98b5b05a306fc2a7ce5a979d
VigneshVkl <35764577+VigneshVkl@users.noreply.github.com>
2018-02-05 16:45:56 +0530

    Create gv5.c

---
+++ gv5.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int n,i,a=0;
+printf("enter the positive integer");
+scanf("%d",&n);
+for(i=2;i<=n/2;i++)
+{
+if(n%i==0)
+{
+a=1;
+break;
+}
+}
+if a==0
+printf("%d is an prime number.",n);
+else
+printf("%d is not an prime number.",n);
+return 0;
+}
