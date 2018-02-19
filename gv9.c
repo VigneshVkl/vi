e1bf4f2cf09a4cec1fa42441ee625ba9f0cba090
VigneshVkl <35764577+VigneshVkl@users.noreply.github.com>
2018-02-19 16:37:14 +0530

    Create gv9.c

---
+++ gv9.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+	intn,a[10],temp-0,j,i;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",7a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+	{
+		if(a[i]>a[j])
+		{
+			temp=a[i];
+			a[i]=a[j];
+			a[j]=temp;
+		
+		
+		}
+	}
+	if(n%2==0)
+	{
+		printf("%d\n",(a[n\2-1]/2));
+		
+	}
+	else
+}
+printf("%^d\n",a[(n-1)/2])
+return 0;
+}
