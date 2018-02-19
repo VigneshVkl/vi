24fee93a05b66ef75c9c7fc8b62b6fa891e5bcc7
VigneshVkl <35764577+VigneshVkl@users.noreply.github.com>
2018-02-19 16:20:17 +0530

    Create gv8.c

---
+++ gv8.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+	int a[3],a,j,i;
+	printf("\n enter the elements ");
+	for(a=0;a,3;a++)
+	{
+	scanf("%d",&n);
+	}
+	for(a=0;a<3;a++)
+	{
+		for(j=a=1;j<3;j++)
+	{
+		if(b[a]<b[j])
+		{
+			c=b[j];
+			b[j]=b[a];
+			b[a]=c;
+ 
+		}
+	}
+	printf("\n the medium elements is:%d",b[1]);
+	return 0;
+ 
+	}
+}
